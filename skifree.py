70e87ccc9d0f83b18f1e7f6be29e254784c12655
Jesse Klein <jklein08@gmail.com>
2015-05-03 11:23:52 -0700

    Modified code
    Removed some lines that were not used

--- skifree.py
+++ skifree.py
@@ -5,12 +5,7 @@
 # http://www.manning-source.com/books/sande/All_Files_By_Chapter/hw_ch10_code/skiing_game.py
 # Released under the MIT license http://www.opensource.org/licenses/mit-license.php
 
-import pygame
-import sys
-import os
-import random
-import urllib
-import urllib2
+import pygame, sys, os, random, urllib, urllib2
 
 skier_images = ["images/skier_down.png",
                 "images/skier_right1.png",
@@ -60,19 +55,19 @@ class ObstacleClass(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = location
         self.type = type
-        self.passed = False
+    
+    def update(self):
+        self.rect.centery -= speed[1]
+        if self.rect.centery < -32:
+            self.kill()
 
-    def scroll(self, terrainPos):
-        self.rect.centery = self.location[1] - terrainPos
-
-def create_map(start, end):
-    obstacles = pygame.sprite.Group()
-    gates = pygame.sprite.Group()
+def create_map():
+    global obstacles
     locations = []
     for i in range(10):
-        row = random.randint(start, end)
+        row = random.randint(0, 9)
         col = random.randint(0, 9)
-        location = [col * 64 + 20, row * 64 + 20]
+        location = [col * 64 + 20, row * 64 + 20 + 640]
         if not (location in locations):
             locations.append(location)
             type = random.choice(["tree", "flag"])
@@ -80,67 +75,49 @@ def create_map(start, end):
             elif type == "flag": img = "images/skier_flag.png"
             obstacle = ObstacleClass(img, location, type)
             obstacles.add(obstacle)
-    return obstacles
 
 def animate():
     screen.fill([255, 255, 255])
-    pygame.display.update(obstacles.draw(screen))
+    obstacles.draw(screen)
     screen.blit(skier.image, skier.rect)
     screen.blit(score_text, [10, 10])
     pygame.display.flip()
-
-def updateObstacleGroup(map0, map1):
-    obstacles = pygame.sprite.Group()
-    for ob in map0: obstacles.add(ob)
-    for ob in map1: obstacles.add(ob)
-    return obstacles
 
 def main():    
     global screen
     global obstacles
     global skier
     global score_text
+    global speed
     pygame.init()
     screen = pygame.display.set_mode([640, 640])
     clock = pygame.time.Clock()
     skier = SkierClass()
     speed = [0, 6]
+    obstacles = pygame.sprite.Group()
     map_position = 0
     points = 0
-    map0 = create_map(20, 29)
-    map1 = create_map(10, 19)
-    activeMap = 0
-    
-    obstacles = updateObstacleGroup(map0, map1)
-    
+    create_map()
     font = pygame.font.Font(None, 50)
     
-    while True:
+    running = True
+    while running:
         clock.tick(30)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT:
+                running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     speed = skier.turn(-1)
                 elif event.key == pygame.K_RIGHT:
                     speed = skier.turn(1)
         skier.move(speed)
+        
         map_position += speed[1]
         
-        if map_position >= 640 and activeMap == 0:
-            activeMap = 1
-            map0 = create_map(20, 29)
-            obstacles = updateObstacleGroup(map0, map1)
-        if map_position >= 1280 and activeMap == 1:
-            activeMap = 0
-            for ob in map0:
-                ob.location[1] = ob.location[1] - 1280
-            map_position = map_position - 1280
-            map1 = create_map(10, 19)
-            obstacles = updateObstacleGroup(map0, map1)
-            
-        for obstacle in obstacles:
-            obstacle.scroll(map_position)
+        if map_position >= 640:
+            create_map()
+            map_position = 0
             
         hit = pygame.sprite.spritecollide(skier, obstacles, False)
         if hit:
@@ -178,8 +155,10 @@ def main():
                 points += 10
                 hit[0].kill()
         
+        obstacles.update()
         score_text = font.render("Score: " + str(points), 1, (0, 0, 0))
         animate()
 
 if __name__ == "__main__":
     main()
+    pygame.quit()
